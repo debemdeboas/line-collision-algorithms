fd10a20d366e9358f2dd95e686afd8f88f858f97
Rafa de Bem <35022953+debemdeboas@users.noreply.github.com>
2021-09-20 21:12:11 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Collision Detection Algorithm'
+project = 'Collision Detection Algorithms'
 copyright = '2021, Gustavo Geyer, Marcio Sarroglia Pinho, Rafael Almeida de Bem'
 author = 'Gustavo Geyer, Marcio Sarroglia Pinho, Rafael Almeida de Bem'
 
